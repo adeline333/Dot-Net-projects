efa60c99e88ba4a867a4dfe30e57d91e26aa6852
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2025-04-08 12:08:24 +0200

    simple BMI calc in c#

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BMIcalc
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("welcome to the BMI calculator");
+            double weight = getWeight();
+            double height = GetHeight();
+            double BMI = calculateBMI(weight, height);
+            Console.WriteLine("your final weight is" + Math.Round(BMI, 2));
+
+
+        }
+        static double getWeight()
+        {
+            Console.WriteLine("enter your weight in pounds");
+            double weight = double.Parse(Console.ReadLine());
+            return weight;
+
+        }
+
+        static double GetHeight()
+        {
+            Console.WriteLine("enter your height in feet and inches (ex:5'8\"): ");
+            String input = Console.ReadLine();
+            String[] split = input.Split("'");
+            double feet = double.Parse(split[0]);
+            double inches = Double.Parse(split[1].Trim('"').Trim());
+            double height = (feet * 12) + inches;
+            return height;
+
+
+        }
+        static double calculateBMI(double weight, double height)
+        {
+            //convert inches to meters
+            double meterHeight = height * 0.0254;
+            //convert our weight into kilograms
+            double kilogramWeight = weight * 0.453592;
+            double bmi = kilogramWeight / (meterHeight * meterHeight);
+            return bmi;
+        }
+
+    }
+}
+
