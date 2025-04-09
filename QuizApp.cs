066eb61a54df2ae1491dd9c42d2484e633c5e33b
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2025-04-09 12:07:18 +0200

    questions and answers array for the quiz app

---
+++ QuizApp.cs
@@ -0,0 +1,24 @@
+
+class QuizProgram
+{
+    static void Main()
+    {    
+
+         //create questions
+        string[] questions =
+        {
+            "what book holds the record for the fastest selling book in history",
+            " How old was queen Elisabeth II when she wa first crowned the Quee of England",
+             "Which bloodtype is a universal donor?"
+        };
+
+        //create Answers
+        string[] answers =
+        {
+            "A.The hunger games \n B.Harry Potter \n C.kjhkhkljhkhkjh\n",
+            "A.27 \n  B. 24 \n C. 31\n",
+            "A. O negative\n B. B positive \n C. AB\n"
+        };
+
+    }
+    }
